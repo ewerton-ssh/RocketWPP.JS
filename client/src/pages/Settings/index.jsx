8b59405d96d88019a27ba6d9bf8052e1ba65235e
Ewerton Lima <88050094+ewerton-ssh@users.noreply.github.com>
2024-01-31 23:20:06 -0300

    Update index.jsx

--- client/src/pages/Settings/index.jsx
+++ client/src/pages/Settings/index.jsx
@@ -51,6 +51,7 @@ export default function Settings() {
                         <label>IP/Port</label>
                         <input type="text"  placeholder='IP/Port' value={ip} onChange={(e) => setIp(e.target.value)}/>
                         <p>Webhook: http://adress:port/rocketjs-webhook</p>
+                        <p>For integration, use "enviawpp number, msg</p>
                         <button className='loginButton' type="submit" onClick={handleSettings}>Save</button>
                     </form>
                 </div>
