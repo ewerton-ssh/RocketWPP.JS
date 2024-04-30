a6b9b9b60d3062178c5f08e535d98fd4c75afca8
Ewerton Lima <88050094+ewerton-ssh@users.noreply.github.com>
2024-04-30 09:09:13 -0300

    Update version description.jsx

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <>
             <div className="footer-container">
-                <p className='version'>Version 1.4.0</p>
+                <p className='version'>Version 1.5.1</p>
                 <p className='version'><FaGithub /> Dev <a className="version" target="blank" href="https://github.com/ewerton-ssh/">ewerton.ssh</a></p>
             </div>
         </>
